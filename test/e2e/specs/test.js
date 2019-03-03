742a9e2ba4b2083c58c7bd8df66601649aa7b9c6
Jesús A. Canales <jesus.canales@socialpoint.es>
2019-03-03 21:04:35 +0100

    Fix 2e2 tests

--- test/e2e/specs/test.js
+++ test/e2e/specs/test.js
@@ -11,9 +11,9 @@ module.exports = {
     browser
       .url(devServer)
       .waitForElementVisible('#app', 5000)
-      .assert.elementPresent('.hello')
-      .assert.containsText('h1', 'Welcome to Your Vue.js App')
-      .assert.elementCount('img', 1)
+      .assert.elementPresent('.presentation')
+      .assert.containsText('h1', 'Jesús Antonio Canales Diez')
+      .assert.elementCount('aside', 1)
       .end();
   },
 };
