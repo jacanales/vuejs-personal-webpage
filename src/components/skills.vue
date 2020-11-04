d53c9e0db7c6daafbe83f7cee28a0936d44c22df
Jesús Antonio Canales Diez <jacanalesdiez@gmail.com>
2020-11-04 22:11:54 +0100

    Add basic skills template

---
+++ src/components/skills.vue
@@ -0,0 +1,65 @@
+<template>
+    <section class="skills">
+        <div class="hero-body">
+            <h2 class="subtitle is-1">
+                Skills
+            </h2>
+            <nav class="tabs centered">
+                <div class="columns is-variable is-mobile is-multiline social-list">
+                    <div class="column">
+                        <figure class="image skill is-96x96">
+                            <img class="is-rounded" src="images/skills/golang.png">
+                        </figure>
+                    </div>
+                    <div class="column">
+                        <figure class="image skill is-96x96">
+                            <img class="is-rounded" src="images/skills/php7.png">
+                        </figure>
+                    </div>
+                    <div class="column">
+                        <figure class="image skill is-96x96">
+                            <img class="is-rounded" src="images/skills/aws.png">
+                        </figure>
+                    </div>
+                    <div class="column">
+                        <figure class="image skill is-96x96">
+                            <img class="is-rounded" src="images/skills/mysql.png">
+                        </figure>
+                    </div>
+                    <div class="column">
+                        <figure class="image skill is-96x96">
+                            <img class="is-rounded" src="images/skills/docker.png">
+                        </figure>
+                    </div>
+                    <div class="column">
+                        <figure class="image skill is-96x96">
+                            <img class="is-rounded" src="images/skills/kubernetes.png">
+                        </figure>
+                    </div>
+                </div>
+            </nav>
+        </div>
+    </section>
+</template>
+
+<script>
+    export default {
+        name: 'Skills'
+    };
+</script>
+
+<style lang="scss">
+    h2.subtitle {
+        text-align: center;
+    }
+
+    figure.skill {
+        margin: 10px;
+        img {
+            height: 100%;
+            box-shadow:0 0 10px 1px #000;
+            -moz-box-shadow:0 0 10px 1px #000;
+            -webkit-box-shadow:0 0 10px 1px #000;
+        }
+    }
+</style>
