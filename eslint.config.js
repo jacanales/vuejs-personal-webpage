3d5ab7dd086bb1bb1156f7fc56dbca5f58ef8872
Jesús Antonio Canales Diez <jacanalesdiez@gmail.com>
2026-01-04 20:22:51 +0100

    feat: bump eslint to 9.39.2

---
+++ eslint.config.js
@@ -0,0 +1,34 @@
+import pluginVue from 'eslint-plugin-vue'
+import vueTsEslintConfig from '@vue/eslint-config-typescript'
+import pluginPlaywright from 'eslint-plugin-playwright'
+import skipFormatting from '@vue/eslint-config-prettier/skip-formatting'
+
+export default [
+  {
+    name: 'app/files-to-lint',
+    files: ['**/*.{ts,mts,tsx,vue}']
+  },
+
+  {
+    name: 'app/ignores',
+    ignores: [
+      '**/dist/**',
+      '**/dist-ssr/**',
+      '**/coverage/**',
+      '**/node_modules/**',
+      '**/.vscode/**',
+      '**/.idea/**',
+      '**/*.log'
+    ]
+  },
+
+  ...pluginVue.configs['flat/essential'],
+  ...vueTsEslintConfig(),
+
+  {
+    ...pluginPlaywright.configs['flat/recommended'],
+    files: ['e2e/**/*.{test,spec}.{js,ts,jsx,tsx}']
+  },
+
+  skipFormatting
+]
--- src/components/jacd/skills.vue
+++ src/components/jacd/skills.vue
@@ -1,78 +1,80 @@
 <template>
-    <section class="skills hidden">
-        <div class="hero-body">
-            <h2 class="subtitle is-1">
-                Skills
-            </h2>
-            <nav class="tabs centered">
-                <div class="columns is-variable is-mobile is-multiline social-list">
-                    <div class="column">
-                        <figure class="image skill is-96x96">
-                            <img class="is-rounded" src="@/assets/images/skills/golang.png" title="golang">
-                        </figure>
-                    </div>
-                    <div class="column">
-                        <figure class="image skill is-96x96">
-                            <img class="is-rounded" src="@/assets/images/skills/php7.png" title="php7">
-                        </figure>
-                    </div>
-                    <div class="column">
-                        <figure class="image skill is-96x96">
-                            <img class="is-rounded" src="@/assets/images/skills/aws.png" title="aws">
-                        </figure>
-                    </div>
-                    <div class="column">
-                        <figure class="image skill is-96x96">
-                            <img class="is-rounded" src="@/assets/images/skills/mysql.png" title="mysql">
-                        </figure>
-                    </div>
-                    <div class="column">
-                        <figure class="image skill is-96x96">
-                            <img class="is-rounded" src="@/assets/images/skills/docker.png" title="docker">
-                        </figure>
-                    </div>
-                    <div class="column">
-                        <figure class="image skill is-96x96">
-                            <img class="is-rounded" src="@/assets/images/skills/kubernetes.png" title="kubernetes">
-                        </figure>
-                    </div>
-                </div>
-            </nav>
+  <section class="skills hidden">
+    <div class="hero-body">
+      <h2 class="subtitle is-1">Skills</h2>
+      <nav class="tabs centered">
+        <div class="columns is-variable is-mobile is-multiline social-list">
+          <div class="column">
+            <figure class="image skill is-96x96">
+              <img class="is-rounded" src="@/assets/images/skills/golang.png" title="golang" />
+            </figure>
+          </div>
+          <div class="column">
+            <figure class="image skill is-96x96">
+              <img class="is-rounded" src="@/assets/images/skills/php7.png" title="php7" />
+            </figure>
+          </div>
+          <div class="column">
+            <figure class="image skill is-96x96">
+              <img class="is-rounded" src="@/assets/images/skills/aws.png" title="aws" />
+            </figure>
+          </div>
+          <div class="column">
+            <figure class="image skill is-96x96">
+              <img class="is-rounded" src="@/assets/images/skills/mysql.png" title="mysql" />
+            </figure>
+          </div>
+          <div class="column">
+            <figure class="image skill is-96x96">
+              <img class="is-rounded" src="@/assets/images/skills/docker.png" title="docker" />
+            </figure>
+          </div>
+          <div class="column">
+            <figure class="image skill is-96x96">
+              <img
+                class="is-rounded"
+                src="@/assets/images/skills/kubernetes.png"
+                title="kubernetes"
+              />
+            </figure>
+          </div>
         </div>
-    </section>
+      </nav>
+    </div>
+  </section>
 </template>
 
-<script>
-    export default {
-        name: 'owned_skills'
-    };
+<script lang="ts">
+export default {
+  name: 'owned_skills'
+}
 </script>
 
 <style lang="scss">
-    h2.subtitle {
-        text-align: center;
-    }
+h2.subtitle {
+  text-align: center;
+}
 
-    figure.skill {
-        margin: 10px;
-        img {
-            height: 100%;
-            box-shadow:0 0 10px 1px #000;
-            -moz-box-shadow:0 0 10px 1px #000;
-            -webkit-box-shadow:0 0 10px 1px #000;
-        }
-    }
+figure.skill {
+  margin: 10px;
+  img {
+    height: 100%;
+    box-shadow: 0 0 10px 1px #000;
+    -moz-box-shadow: 0 0 10px 1px #000;
+    -webkit-box-shadow: 0 0 10px 1px #000;
+  }
+}
 
-    section.hidden {
-        display: none;
-    }
+section.hidden {
+  display: none;
+}
 
-    .columns.is-variable {
-      --columnGap: 0;
-    }
+.columns.is-variable {
+  --columnGap: 0;
+}
 
-    .columns.is-variable .column {
-      padding-left: var(--columnGap);
-      padding-right: var(--columnGap);
-    }
+.columns.is-variable .column {
+  padding-left: var(--columnGap);
+  padding-right: var(--columnGap);
+}
 </style>
